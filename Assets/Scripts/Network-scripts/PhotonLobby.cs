ee37ba4690686fb047956cb837c7c400c0ce36f6
Tilda Hylander <tilhy349@student.liu.se>
2021-02-24 13:46:15 +0100

    PhotonLobby test

--- Assets/Scripts/Network-scripts/PhotonLobby.cs
+++ Assets/Scripts/Network-scripts/PhotonLobby.cs
@@ -9,9 +9,10 @@ public class PhotonLobby : MonoBehaviourPunCallbacks
     public static PhotonLobby lobby; //singleton = template of instances
 
     public GameObject battleButton; //match-making system sliterio, clash royale
+    public GameObject cancelButton;
 
-    public int roomNumber;
-    RoomInfo[] rooms;
+    //public int roomNumber;
+    //RoomInfo[] rooms;
 
     private void Awake()
     {
@@ -34,20 +35,35 @@ public class PhotonLobby : MonoBehaviourPunCallbacks
 
     public void OnBattleButtonClicked()
     {
+        battleButton.SetActive(false);
+        cancelButton.SetActive(true);
         PhotonNetwork.JoinRandomRoom(); //picks random room
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         Debug.Log("Tried to join join a random game but failed. There must be no open games available");
+        CreateRoom();
+    }
+
+    void CreateRoom()
+    {
         int randomRoomName = Random.Range(0, 10000);
         RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 10 }; //Room specifications
         PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOps);
     }
 
-    void CreateRooms()
+    public override void OnCreateRoomFailed(short returnCode, string message)
     {
+        Debug.Log("Tried to create a new room, there must already be room with the same name");
+        CreateRoom();
+    }
 
+    public void OnCancelButtonClicked()
+    {
+        cancelButton.SetActive(false);
+        battleButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
     }
 
     // Update is called once per frame
