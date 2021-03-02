8f649173fc6f46f6f67c9abab476e1d67ecaf9fc
emibe986 <emibe986@student.liu.se>
2021-03-02 16:54:08 +0100

    LobbySystem tweaks

--- Assets/Scripts/Network-scripts/TeamPickController.cs
+++ Assets/Scripts/Network-scripts/TeamPickController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TeamPickController : MonoBehaviour
 {
+    public Button[] teamButtons;
+
     public void OnClickTeamPick(int clickedTeam) 
     {
         if (PlayerInfo.PI != null)
@@ -11,6 +14,12 @@ public class TeamPickController : MonoBehaviour
             PlayerInfo.PI.mySelectedTeam = clickedTeam;
             PlayerPrefs.SetInt("MyTeam", clickedTeam);
             Debug.Log("Im in team" + clickedTeam);
+
+            teamButtons[0].image.fillCenter = false;
+            teamButtons[1].image.fillCenter = false;
+
+            teamButtons[clickedTeam].image.fillCenter = true;
+            
         }
     }
 }
